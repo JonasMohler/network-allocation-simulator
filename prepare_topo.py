ae77eb56e35635110fb24fe25e613f5a9a6c6dbf
jmohler <jmohler@student.ethz.ch>
2021-06-20 16:05:29 +0200

    zoo

---
+++ prepare_topo.py
@@ -0,0 +1,40 @@
+import os
+from argparse import ArgumentParser
+
+import fnss
+import networkx as nx
+import src.util.data_handler as dh
+
+DATA = "dat/topologyzoo"
+OUT = "dat/topologies"
+
+for filename in os.listdir(DATA):
+    if filename.endswith("graphml"):
+        print("---")
+        # Load the graph
+        graph = nx.read_graphml(os.path.join(DATA, filename))
+        # Check if it is fully connected
+        suffix = ""
+        if not nx.is_connected(graph):
+            # Keep only the largest connected component
+            print(f"Original graph size: {len(graph.nodes())}")
+            connected_nodes = max(nx.connected_components(graph), key=len)
+            graph = graph.subgraph(connected_nodes).copy()
+            suffix = "-GC"
+            print(
+                f"KEEPING only largest connected component of size: {len(graph.nodes())}"
+            )
+        # Save at xml
+        name = filename.rsplit(".", maxsplit=1)[0]
+        name = f"{name}{suffix}({len(graph.nodes())})"
+        print(name)
+        topo = os.path.join(OUT, name)
+        if not os.path.exists(topo):
+            os.mkdir(topo)
+
+        graph_path = os.path.join(topo, 'graph/')
+        if not os.path.exists(graph_path):
+            os.mkdir(graph_path)
+
+        graph = fnss.Topology(graph)
+        fnss.write_topology(graph, os.path.join(graph_path, "topology.xml"))
