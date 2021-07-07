0d74bdb55f72bd591bac4f0f7e52402ef9568693
jmohler <jmohler@student.ethz.ch>
2021-07-07 15:43:01 +0200

    plot core

--- src/util/plot_handler.py
+++ src/util/plot_handler.py
@@ -192,9 +192,10 @@ def make_fig_split(x_name, y_name, data, title, strategies, p_type='scatter', sa
         name = f"{name}_b_split.{PLOT_FORMAT}"
 
         i = 0
+        print(f"Strat Column: {data['Strategy']}")
         for s in strategies:
             #print(f'Strat Label: {s}')
-            print(f"Strat Column: {data['Strategy']}")
+            print(f"Strat Column selected: {data[data['Strategy'] == STRATEGY_LABEL[s]]}")
             print(f"Data: {data[(data['Strategy'] == s)][['Allocations Gbps', 'Strategy']]}")
             sns.boxplot(data=data[(data["Strategy"] == s)], x=x_name, y=y_name, hue="Strategy",
                             ax=axs[i], palette=C_MAP)
