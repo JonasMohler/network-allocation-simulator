dc6756a8d9ade2e27aa2a0698b5cb9a88ddc7c06
jmohler <jmohler@student.ethz.ch>
2021-07-17 12:51:04 +0200

    plot core

---
+++ make_rands_const.py
@@ -0,0 +1,21 @@
+import os
+from src.multiprocessing.topology.PerTopologyOperations import AddConstantCapacity
+from subprocess import call
+
+def cp_dir(source, target):
+    call(['cp', '-a', source, target])
+
+TOPO = 'data2/jmohler/dat/topologies'
+
+for dir in os.listdir(TOPO):
+    if dir.startswith('Barabasi'):
+        cp_dir(dir, f"c_{dir}")
+
+tops = []
+for dir in os.listdir(TOPO):
+    if dir.startswith('c_Barabasi'):
+        tops.append(dir)
+
+proc = AddConstantCapacity(tops, 70, 15)
+proc.run()
+
